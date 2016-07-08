0c02d8ccbbc2df24f0a7c660e78ed76411ce24d8
paulozag <ozag@comcast.net>
2016-07-08 14:22:39 -0700

    completes js portion of prob 1.6

--- questions/question_1.6/problem_1.6.js
+++ questions/question_1.6/problem_1.6.js
@@ -1,3 +1,29 @@
+
+var rotateImage = function(arr){
+  var length = arr.length
+  var startIndex = 0
+  var endIndex = length - 1
+
+   while(endIndex - startIndex > 1){
+    rotateShell(arr, startIndex, endIndex)
+    startIndex++
+    endIndex--
+  }
+};
+
+var rotateShell = function(array, first, last){
+
+  for (var index = first; index < last; index++){
+    // rotates each row elemnt by 90 degrees for all elements in the outer 'shell' of the array
+    var temp                      = array[first][first + index]
+    array[first][first +index]    = array[last - index][first]
+    array[last - index][first]    = array[last][last - index]
+    array[last][last - index]     = array[first + index][last]
+    array[first + index][last]    = temp
+  };
+};
+// helper/testing methods
+
 var createNByNMatrix = function(size){
   var arr = []
   for (var i = 1; i <= size; i++){
@@ -13,7 +39,6 @@ var createNByNMatrix = function(size){
 var isValidNByN = function(arr){
   var depth = arr.length
   for (var i = 0; i < arr.length; i++){
-    // console.log(arr[i].length + '   ' + depth)
     if (arr[i].length != depth){
       return false;
     }
@@ -24,10 +49,12 @@ var isValidNByN = function(arr){
 var displayMatrix = function(arr){
   for (i in arr){
     console.log(arr[i].join(' '));
-  }
+  };
 };
 
 mat = createNByNMatrix(5)
-// console.log(mat[1].length)
-console.log(isValidNByN(mat))
+
+displayMatrix(mat)
+console.log('XXXXXXXXXXXXXXXXXXXXXXXXX')
+rotateImage(mat)
 displayMatrix(mat)
