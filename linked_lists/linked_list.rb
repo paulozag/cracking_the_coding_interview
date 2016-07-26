7a121a13e896e7b1da586caa0107702ee2a86b50
paulozag <ozag@comcast.net>
2016-07-26 16:11:14 -0700

    adds ruby code for LL class

--- linked_lists/linked_list.rb
+++ linked_lists/linked_list.rb
@@ -41,6 +41,42 @@ class Linked_list
     count
   end
 
+  def remove_node target
+    if target == head
+      @head = head.next
+      return
+    end
+    previous = head
+    runner = head.next
+    while runner
+      if runner == target
+        previous.next = runner.next
+        return runner
+      end
+      previous = runner
+      runner = runner.next
+    end
+    nil
+  end
+
+  def contains_node? node
+    runner = head
+    while runner
+      return true if runner == node
+      runner = runner.next
+    end
+    nil
+  end
+
+  def contains_value? target
+    runner = head
+    while runner
+      return true if runner.value == target
+      runner = runner.next
+    end
+    nil
+  end
+
 end
 
 class Node
@@ -52,11 +88,11 @@ class Node
   end
 end
 
-x = Linked_list.new (Node.new(5))
-x.view_list
-x.add_node Node.new(4)
-p x.last_element.value
-x.add_node_by_value 3
-x.view_list
-p "length #{x.length}"
+# x = Linked_list.new (Node.new(5))
+# x.view_list
+# x.add_node Node.new(4)
+# p x.last_element.value
+# x.add_node_by_value 3
+# x.view_list
+# p "length #{x.length}"
 # !!(Node.new(2).next)
