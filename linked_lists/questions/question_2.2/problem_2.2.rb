f04a2e97c46a195de5b0516a0d66bfaf29af4379
paulozag <ozag@comcast.net>
2016-08-01 16:15:44 -0700

    prints 2.2.rb node value but still cant pass it by reference

--- linked_lists/questions/question_2.2/problem_2.2.rb
+++ linked_lists/questions/question_2.2/problem_2.2.rb
@@ -1,33 +1,13 @@
 require '../../linked_list.rb'
 
-# class LinkedList
+def find_kth_to_last k
+  find_kth head, k
+end
 
-#   def kth_to_last_element k
-#     list_length = self.length
-#     p "length of list: #{list_length}"
-#     list_count = 0
-#     find_kth head,k, listcount, false
-#   end
+def find_kth head, k
+  return 0 unless head
 
-#   def find_kth current, target, list_count, found_flag
-#     return current if found_flag
+  p head.value if (find_kth head.next, k) + 1 == k
 
-#     if list_count > 1
-#       if list_count == target
-#         found_flag = true
-#         return current
-#       end
-#       list_count += 1
-#       return
-#     end
-
-#     if !current.next
-#       list_count = 1
-#       return
-#     end
-
-#     return find_kth current.next, target, list_count, found_flag
-#   end
-
-# end
+end
 
