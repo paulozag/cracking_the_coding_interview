eea2341573899bdeaccea794e43c833b98bf3ca0
paulozag <ozag@comcast.net>
2016-09-01 14:37:27 -0700

    refactors recursive flatten algorithm

--- experiments/flatten_recur.rb
+++ experiments/flatten_recur.rb
@@ -2,14 +2,7 @@ def recur_flatten arr
   package = []
   return [arr] unless arr.class == Array
   arr.each do |element|
-
     element.class == Array ? package += (recur_flatten element) : package << element
-
-    # if element.class == Array
-    #   package += recur_flatten element
-    # else
-    #   package << element
-    # end
   end
   package
 end
