cf7f96d0fd7dd1c437e2b3de28d610f07b795a0f
paulozag <ozag@comcast.net>
2016-09-19 14:58:09 -0700

    finishes 3.4.rb

--- stacks_and_queues/questions/question_3.4/problem_3.4.rb
+++ stacks_and_queues/questions/question_3.4/problem_3.4.rb
@@ -42,7 +42,7 @@ class Disc
 end
 
 class Tower
-  attr_reader :tower_1
+  attr_reader :tower_1, :tower_2, :tower_3
 
   def initialize num_discs=9
     @tower_1 = Stack.new
@@ -51,6 +51,23 @@ class Tower
     @discs = []
     create_discs num_discs
     load_first_tower_with_discs
+  end
+
+  def solve
+    move @discs.count, @tower_1, @tower_2, @tower_3
+  end
+
+  def move num_discs, from, pivot, destination
+    # move num_discs -1 from from to pivot
+    #  move last disc from from to destination
+    #  move num_discs -1 from pivot to destination
+    if num_discs == 1
+      destination.push from.pop
+    else
+      move num_discs -1, from, destination, pivot
+      move 1, from, pivot, destination
+      move num_discs -1, pivot, from, destination
+    end
   end
 
 
@@ -68,3 +85,8 @@ end
 
 
 tower = Tower.new
+p tower.tower_1.peek
+tower.solve
+p tower.tower_1.peek
+p tower.tower_3.peek
+
