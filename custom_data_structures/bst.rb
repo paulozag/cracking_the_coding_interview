15dd0771efdfe303813992504a379088d5eb94b1
paulozag <ozag@comcast.net>
2016-10-14 16:35:07 -0700

    debugs bst#remove, now works for removing a node w/ 2 children

--- custom_data_structures/bst.rb
+++ custom_data_structures/bst.rb
@@ -1,4 +1,5 @@
 require_relative 'tree_node'
+require 'pry'
 
 class BinarySearchTree
   attr_accessor :root
@@ -32,19 +33,10 @@ class BinarySearchTree
 
   def remove(target)
     if @root.value == target
-      unless @root.left || @root.right
-        @root.value = nil
-        return target
-      end
-
-      if @root.left
-        @root.value = find_and_delete_max_child(@root.left, @root)
-      else
-        @root.value = find_and_delete_min_child(@root.right, @root)
-      end
-
+      remove_root_node
       return target
     end
+
     if target < root.value
       root.left ? delete_target(target, root.left, root) : (return nil)
     else
@@ -55,26 +47,47 @@ class BinarySearchTree
 
 
   private
+  def remove_root_node
+    unless @root.left || @root.right
+      @root.value = nil
+    end
+
+    if @root.left
+      @root.value = find_and_delete_max_child(@root.left, @root)
+    else
+      @root.value = find_and_delete_min_child(@root.right, @root)
+    end
+  end
+
   def delete_target(target, root, parent)
+    # binding.pry
     if target == root.value
       delete_node(root, parent)
       return target
     elsif target < root.value
       root.left ? delete_target(target, root.left, root) : (return nil)
-    elsif target > root.value
+    else
       root.right ? delete_target(target, root.right, root) : (return nil)
     end
   end
 
   def delete_node(node_to_delete, parent)
+    # binding.pry
     return_value = node_to_delete.value
     case child_count(node_to_delete)
-    when 0, 1
+    when 0
       if parent.left == node_to_delete
         parent.left = nil
       else
         parent.right = nil
       end
+    when 1
+      if parent.left == node_to_delete
+        node_to_delete.left ? (parent.left = node_to_delete.left) : (parent.left = node_to_delete.right)
+      else
+        node_to_delete.left ? (parent.right = node_to_delete.left) : (parent.right = node_to_delete.right)
+      end
+
     when 2
       node_to_delete.value = find_and_delete_min_child(node_to_delete.right, node_to_delete)
     end
@@ -82,9 +95,11 @@ class BinarySearchTree
   end
 
   def find_and_delete_min_child(root, parent)
+    # binding.pry
     unless root.left
-      parent.left = nil
-      return root.value
+      return_value = root.value
+      delete_node(root, parent)
+      return return_value
     end
     find_and_delete_min_child(root.left, root)
   end
@@ -112,10 +127,14 @@ a.insert 1
 a.insert 3
 a.insert 7
 a.insert 9
+a.insert 0.5
+a.insert 4
+a.insert 3.5
+a.insert 4.5
 
 a.print_tree
 p '************************'
-p a.remove(7)
+p a.remove(2)
 p '************************'
 a.print_tree
 p "new root: #{a.root.value}"
