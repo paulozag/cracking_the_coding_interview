2a33e483a4b4e27b5a396938a9c2054ab950b286
paulozag <ozag@comcast.net>
2016-10-14 15:06:24 -0700

    adds remove to bst, needs refactor and testing

--- custom_data_structures/bst.rb
+++ custom_data_structures/bst.rb
@@ -29,14 +29,93 @@ class BinarySearchTree
     p root.value
     print_tree(root.right) if root.right
   end
+
+  def remove(target)
+    if @root.value == target
+      unless @root.left || @root.right
+        @root.value = nil
+        return target
+      end
+
+      if @root.left
+        @root.value = find_and_delete_max_child(@root.left, @root)
+      else
+        @root.value = find_and_delete_min_child(@root.right, @root)
+      end
+
+      return target
+    end
+    if target < root.value
+      root.left ? delete_target(target, root.left, root) : (return nil)
+    else
+      root.right ? delete_target(target, root.right, root) : (return nil)
+    end
+  end
+
+
+
+  private
+  def delete_target(target, root, parent)
+    if target == root.value
+      delete_node(root, parent)
+      return target
+    elsif target < root.value
+      root.left ? delete_target(target, root.left, root) : (return nil)
+    elsif target > root.value
+      root.right ? delete_target(target, root.right, root) : (return nil)
+    end
+  end
+
+  def delete_node(node_to_delete, parent)
+    return_value = node_to_delete.value
+    case child_count(node_to_delete)
+    when 0, 1
+      if parent.left == node_to_delete
+        parent.left = nil
+      else
+        parent.right = nil
+      end
+    when 2
+      node_to_delete.value = find_and_delete_min_child(node_to_delete.right, node_to_delete)
+    end
+    return_value
+  end
+
+  def find_and_delete_min_child(root, parent)
+    unless root.left
+      parent.left = nil
+      return root.value
+    end
+    find_and_delete_min_child(root.left, root)
+  end
+
+  def find_and_delete_max_child(root, parent)
+    unless root.right
+      parent.right = nil
+      return root.value
+    end
+    find_and_delete_max_child(root.right, root)
+  end
+
+  def child_count(node)
+    return 2 if (node.left && node.right)
+    return 1 if (node.left || node.right)
+    0
+  end
 end
 
-a = BinarySearchTree.new(5)
-# p a.root.value
-a.insert(3)
-a.insert(6)
-# a.print_tree
+a = BinarySearchTree.new(6)
+
+a.insert 2
+a.insert 8
+a.insert 1
+a.insert 3
+a.insert 7
 a.insert 9
-a.insert 11
-a.insert 0
+
 a.print_tree
+p '************************'
+p a.remove(7)
+p '************************'
+a.print_tree
+p "new root: #{a.root.value}"
