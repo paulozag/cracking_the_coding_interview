cdc662c0ee217f06d62bdf92e3ec164f5ea2c63f
paulozag <ozag@comcast.net>
2016-10-31 10:32:01 -0700

    implements get_stack_count correctly in 3.6.rb

--- stacks_and_queues/questions/question_3.6/problem_3.6.rb
+++ stacks_and_queues/questions/question_3.6/problem_3.6.rb
@@ -3,6 +3,8 @@
 # current_min
 # stack_size
 
+require 'pry'
+
 class Node
   attr_accessor :value, :next
 
@@ -39,17 +41,20 @@ class Stack
 end
 
 class StackSort
-  attr_reader :unsorted_stack, :temp_stack
+  attr_reader :unsorted_stack, :temp_stack, :stack_size, :perm_stack_count
 
   def initialize unsorted_stack
     @unsorted_stack     = unsorted_stack
     @temp_stack         = Stack.new
-    @stack_size         = 0
+    @stack_size         = get_stack_count
     @perm_stack_count   = 0
   end
 
   def sort
-    set_first_element
+    until @perm_stack_count == @stack_size
+      push_min_element_to_unsorted_stack
+      roll_back_unsorted_stack
+    end
   end
 
   private
@@ -69,6 +74,46 @@ class StackSort
     @unsorted_stack.push(min)
     @perm_stack_count +=  1
   end
+
+  def sort_stack
+    until @perm_stack_count == @stack_size
+      roll_back_unsorted_stack
+      push_min_element_to_unsorted_stack
+    end
+  end
+
+  def roll_back_unsorted_stack
+    (@stack_size - @perm_stack_count).times{@unsorted_stack.push(@temp_stack.pop)}
+  end
+
+  def push_min_element_to_unsorted_stack
+    # binding.pry
+    min_element = @unsorted_stack.pop
+    unsorted_element_count = (@stack_size - @perm_stack_count -1)
+    unsorted_element_count.times do
+      next_element = @unsorted_stack.pop
+      if next_element < min_element
+        @temp_stack.push(min_element)
+        min_element = next_element
+      else
+        @temp_stack.push(next_element)
+      end
+    end
+    @perm_stack_count +=1
+    @unsorted_stack.push(min_element)
+  end
+
+  def get_stack_count
+    count   = 0
+    while(@unsorted_stack.peek)
+      @temp_stack.push(@unsorted_stack.pop)
+      count   +=  1
+    end
+    while(@temp_stack.peek)
+      @unsorted_stack.push(@temp_stack.pop)
+    end
+    count
+  end
 end
 
 us = Stack.new
@@ -81,3 +126,5 @@ s_s = StackSort.new(us)
 s_s.sort
 p s_s.temp_stack
 p s_s.unsorted_stack
+p s_s.stack_size
+p s_s.perm_stack_count
