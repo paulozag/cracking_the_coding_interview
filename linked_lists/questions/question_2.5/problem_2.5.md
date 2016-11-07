0d1eeb7b4c2e8e2205ab50198c8bc3ef4bac5571
paulozag <ozag@comcast.net>
2016-11-06 20:21:00 -0800

    adds LinkedList::convert_arabic_to_linked_list and LinkedList#reverse

--- linked_lists/questions/question_2.5/problem_2.5.md
+++ linked_lists/questions/question_2.5/problem_2.5.md
@@ -1,5 +1,5 @@
 Question 2.5
-You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order, such that the Ts digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order, such that the 1s digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
 EXAMPLE
 Input:(7-> 1 -> 6) + (5 -> 9 -> 2).That is, 617 + 295.
 Output: 2 -> 1 -> 9.That is, 912.
--- linked_lists/questions/question_2.5/problem_2.5.rb
+++ linked_lists/questions/question_2.5/problem_2.5.rb
@@ -1,12 +1,64 @@
-def add_two_lists list1, list2
-  sum = LinkedList.new
-  runner1 = list1.head
-  runner2 = list2.head
-  remainder = 0
+require_relative './../../../custom_data_structures/ruby_data_structures/linked_list.rb'
 
+class LinkedList
 
-  while runner1 || runner2
-    value_of_column = (runner1.value || 0) + (runner2.value || 0) + remainder
-    sum.addNode
+  def self.convert_arabic_to_linked_list(num)
+    list = LinkedList.new
+    while num > 0
+      quotient, remainder = num.divmod(10)
+      list.push(remainder)
+      num = quotient
+    end
+    list
   end
+
+  def reverse
+    temp_list = LinkedList.new
+    runner    = @head
+    while runner
+      temp_list.enqueue(runner.value)
+      runner = runner.next
+    end
+    temp_list
+  end
+
+  def add_two_lists(list1, list2)
+    sum_list  = LinkedList.new
+    runner1   = list1.head
+    runner2   = list2.head
+    carry     = false
+
+    while runner1 || runner2
+      value1, value2        = get_runner_values(runner1, runner2)
+      carry_value           = (carry ? 1 : 0)
+      quotient, remainder   = (value1 + value2 + carry_value).divmod(10)
+      carry                 = (quotient == 1)
+
+      sum_list.push(remainder)
+      runner1, runner2 = advance_runners(runner1, runner2)
+    end
+
+    sum_list.push(1) if carry
+    sum_list
+  end
+
+  private
+  def advance_runners(runner1, runner2)
+    runner1 = runner1.next if runner1
+    runner2 = runner2.next if runner2
+    [runner1, runner2]
+  end
+
+  def get_runner_values(runner1, runner2)
+    value1        = (runner1 ? runner1.value : 0)
+    value2        = (runner2 ? runner2.value : 0)
+    [value1, value2]
+  end
+
 end
+
+list1 = LinkedList.convert_arabic_to_linked_list(3412)
+list1.view_list
+p '**************************************'
+list1 = list1.reverse
+list1.view_list
