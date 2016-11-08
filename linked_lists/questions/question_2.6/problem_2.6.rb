b2554df8d7349a821c9b5c6fc091b4e6a5ae0798
paulozag <ozag@comcast.net>
2016-11-08 11:02:39 -0800

    implements 2.6.rb correctly.  YAY!!!

--- linked_lists/questions/question_2.6/problem_2.6.rb
+++ linked_lists/questions/question_2.6/problem_2.6.rb
@@ -5,40 +5,41 @@ class LinkedList
   def head_of_circular_list
     return nil unless @head && @head.next
     tortoise        = @head
-    rabbit          = @head.next
-    tortoise_steps  = 0
-    circle_size     = nil
+    rabbit          = @head
 
-    while rabbit
-      if rabbit == tortoise
-        break if circle_size
-        circle_size = 0
-      end
+    rabbit, tortoise = collide_runners(rabbit,tortoise)
+    return nil unless rabbit  #acyclic list
+    tortoise  = @head # now both rabbit and tortoise are k steps
+                      # away from circle head
+    head_of_circular_list = have_runners_meet(rabbit, tortoise)
+  end
 
+  def collide_runners(rabbit, tortoise)
+    # when collision occurs, rabbit will be
+    # k (length of non-cyclic front portion) steps
+    # away from the circle head
+    while rabbit && rabbit.next
       tortoise  = tortoise.next
-      rabbit    = advance_rabbit(rabbit)
-      tortoise_steps  += 1
-      circle_size     += 1 if circle_size
+      rabbit    = rabbit.next.next
+      break if rabbit == tortoise
     end
 
-    return nil unless rabbit
-    distance_to_circle_head = tortoise_steps - (2 * circle_size) - 1
-    runner = @head
-    distance_to_circle_head.times {runner = runner.next}
-    runner
+    return [rabbit, tortoise] if rabbit && rabbit.next
+    nil
+  end
+
+  def have_runners_meet(rabbit, tortoise)
+    while true  # move both runners 1 step ahead and they will meet at the
+                # circle head
+      break if tortoise == rabbit
+      rabbit    = rabbit.next
+      tortoise  = tortoise.next
+    end
+    tortoise
   end
 end
 
-x = LinkedList.new
-['A','B','C'].each { |val| x.push(val)}
-head_of_circle = Node.new('D')
-x.push_node(head_of_circle)
-['E','F','G'].each { |val| x.push(val)}
 
-x.view_list
-x.tail.next = head_of_circle
-p '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-# x.view_list
 
 
 
