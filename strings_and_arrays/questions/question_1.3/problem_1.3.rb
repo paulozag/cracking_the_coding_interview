6df59ed63c059851f902bdb3088666e155a418c7
paulozag <ozag@comcast.net>
2016-11-19 17:00:46 -0800

    refactors 1.3.rb

--- strings_and_arrays/questions/question_1.3/problem_1.3.rb
+++ strings_and_arrays/questions/question_1.3/problem_1.3.rb
@@ -1,14 +1,12 @@
-def permutation? word1, word2
-
+def permutation?(word1, word2)
+  histogram(word1) == histogram(word2)
 end
 
-def histo obj
-  case
-  when obj.class == String
-    return obj.split('').inject(Hash.new(0)){ |h,v| h[v] += 1; h }
-  else
-    return -1
-  end
+def histogram(word)
+  histo = Hash.new(0)
+  word.split('').each { |char| histo[char] += 1 }
+  histo
 end
 
-p histo 'hello'
+p histogram('hello')
+p permutation?('melon', 'lemonm')
